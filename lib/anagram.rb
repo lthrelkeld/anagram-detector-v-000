bfc568b2f32e172a067257de94cc9d837cd2c0b2
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 17:32:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,9 @@ class Anagram
   #Returns all anagram matches in the provided array, okr an empty array if there are no matches
   def match(anagram_array)
     matches = []
+    word = @word
     anagram_array.each do |anagram|
-      matches << anagram if anagram.split("").sort.join == @word.split("").sort.join
+      matches << anagram if anagram.split("").sort.join == word.split("").sort.join
     end
     matches
   end
