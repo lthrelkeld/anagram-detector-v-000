d7d5962ff138b56477bc589a47c1cb7b99961a7a
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 17:26:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,5 @@
 class Anagram
+  attr_accessor :word
 
   def initialize(word)
   end
@@ -6,5 +7,9 @@ class Anagram
   #Returns all anagram matches in the provided array, okr an empty array if there are no matches
   def match(anagram_array)
     matches = []
+    anagram_array.each do |anagram|
+      matches << anagram if anagram.split("").sort == @word
+    end
+    
   end
 end
