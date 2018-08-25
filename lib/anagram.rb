d3556bacd390b42c66309ebeaeae890c44d5552d
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 17:35:06 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,12 @@ class Anagram
   #Returns all anagram matches in the provided array, okr an empty array if there are no matches
   def match(anagram_array)
     matches = []
-    word = @word.to_s
+
+    #check if sorted letters of word and anagram match. If so, add to list of matches
     anagram_array.each do |anagram|
       matches << anagram if anagram.split("").sort.join == @word.split("").sort.join
     end
+    
     matches
   end
 end
