463f7c1d99d304557b4b266ae370df3a1f53dc31
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 17:16:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,6 +3,9 @@ class Anagram
   def initialize(word)
   end
 
+  #Returns all anagram matches in the provided array, okr an empty array if there are no matches
   def match(anagram_array)
+    matches = []
+    
   end
 end
