64c40b2c17cd7987c1f8d30ca55ea69ba52f4469
Elizabeth Karst <lkarst@outlook.com>
2018-08-25 17:35:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  #Returns all anagram matches in the provided array, okr an empty array if there are no matches
+  def match(anagram_array)
+    matches = []
+
+    #check if sorted letters of word and anagram match. If so, add to list of matches
+    anagram_array.each do |anagram|
+      matches << anagram if anagram.split("").sort.join == @word.split("").sort.join
+    end
+
+    matches
+  end
+end
